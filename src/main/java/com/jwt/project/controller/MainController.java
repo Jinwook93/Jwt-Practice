135342ad6c3c27e1fa97cd51bf7b6c977274bff8
wook <wook@wook.com>
2025-11-30 20:48:27 +0900

    chatper 12: 세션 정보 불러오기 테스트

--- src/main/java/com/jwt/project/controller/MainController.java
+++ src/main/java/com/jwt/project/controller/MainController.java
@@ -1,5 +1,12 @@
 package com.jwt.project.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +18,18 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "main Controller";
+    	//name
+    	String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    	
+       	//role
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+ 
+    	Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+    	Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+    	GrantedAuthority auth = iter.next();
+    	String role = auth.getAuthority();
+    	
+        return "main Controller" + "/" + username + "/" +role;
     }
 }
